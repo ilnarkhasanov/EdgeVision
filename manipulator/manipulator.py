afd6537189fdf293c36db865ad04ff2c021d76c7
Ilnar <4sitescarp@gmail.com>
2022-12-01 05:08:46 +0300

    Manipulator gets messages

--- manipulator/manipulator.py
+++ manipulator/manipulator.py
@@ -2,16 +2,40 @@ import socket
 import pickle
 from pydantic import BaseModel
 
+from datetime import datetime
+from enum import Enum
+
 
 class SensorData(BaseModel):
     datetime: str
     payload: int
 
 
+class Status(Enum):
+    """
+    This enumeration represents the status of the manipulator.
+    It can be either UP or DOWN.
+    """
+    UP = "up"
+    DOWN = "down"
+
+
+class ControllerSignal(BaseModel):
+    """
+    This pydantic model represents the signals from the controller.
+
+    attributes:
+        datetime: the datetime when the signal was sent
+        status: the status of the manipulator
+    """
+    date_time: datetime
+    status: Status
+
+
 HEADER_SIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost', 1235))
+s.bind(('localhost', 1236))
 s.listen(5)
 
 while True:
@@ -23,8 +47,7 @@ while True:
         msg = client_socket.recv(1024)
 
         if len(msg) <= 0:
-            break
+            continue
 
-        for parsed_signal in pickle.loads(msg):
-            print(SensorData.parse_raw(parsed_signal))
-        print()
+        signal = pickle.loads(msg)
+        print(ControllerSignal.parse_raw(signal))
